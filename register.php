0154ddb4f58d5614c93a29f4d1b7539947dcce76
dr <1950740248>
2017-10-10 21:52:23 +0800

    init

---
+++ register.php
@@ -0,0 +1,23 @@
+<?php
+	header("content-type:text/html;charset:utf-8");
+	//接受客户传来的数据
+	$phone = $_POST['phone'];
+	$pass = $_POST['userPass'];
+	$userName = $_POST['userName'];
+	//处理
+	//1、连接数据库服务器
+	 $con= mysql_connect("localhost","root","root");
+	 	if(!$con){
+	 		echo"注册失败！服务器异常，请稍后再试..."
+	 	}else{
+	 	//2、选择数据库
+	 		mysql_select_db("qiqigedb",$con);
+	 	//3、执行SQL语句
+	 		$sqlStr = "insert into userregister(userPhone,userPass,userName) values('".$phone."','".$pass."','".$userName."')";
+	 		$t = mysql_query($sqlStr.$con);
+	 	//4、关闭数据库	
+	 		mysql_close($con);
+	 	}
+	 	echo $t;
+	
+?>
