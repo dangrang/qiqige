4c5d5f944047612f8ae29e4204b928dc31c40d61
dr <1950740248>
2017-10-11 21:28:37 +0800

    init

--- checkregister.php
+++ checkregister.php
@@ -8,23 +8,22 @@
 	//1、连接数据库服务器
 	 $con= mysql_connect("localhost","root","root");
 	 	if(!$con){
-	 		echo"注册失败！服务器异常，请稍后再试...";
+	 		echo"-1";
 	 	}else{
 	 	//2、选择数据库
 	 		mysql_select_db("qiqigedb",$con);
 	 	//3、执行SQL语句
-	 		$sqlStr = "insert into userregister(userId,userPass,userName) values('".$phone."','".$pass."','".$userName."')";
+//	 		$sqlStr = "insert into userregister(userId,userPass,userName) values('".$phone."','".$pass."','".$userName."')";
+	 		$sqlStr="select * from userregister where userId='".$userId."' and userPass='".$userPass."'";
 	 		$t = mysql_query($sqlStr,$con);
 	 	//4、关闭数据库	
 	 		mysql_close($con);
-	 		
 	 		echo $t;
 	 	}
-	 	
 	 	if($t==1){
-	 		echo"注册成功";
+	 		echo"1";
 	 	}else{
-	 		echo"注册失败";
+	 		echo"0";
 	 	}
 	
 ?>
---
+++ login - 副本.php
@@ -0,0 +1,29 @@
+<?php
+	header("content-type:text/html;charset=utf-8");
+	//接收到用户的输入
+	$userId = $_POST["userId"];
+	$userPass = $_POST["userPass"];
+	//连接数据库的服务器
+	$con= mysql_connect("localhost","root","root");
+	if(!$con){
+		echo "-1";
+	}else{
+		//选择数据库
+		mysql_select_db("qiqigedb",$con);
+		//查询内容
+		$sqlStr="select * from userregister where userId='".$userId."' and userPass='".$userPass."'";
+	   
+	   	$result=mysql_query($sqlStr,$con);
+	   	
+	   	mysql_close($con);
+		
+		//数据库中查询出结果，表示登录成功。
+		if(mysql_num_rows($result)==1){
+			echo "1";
+		}else{
+			echo "0";
+		}	
+	}
+	
+?>
+
